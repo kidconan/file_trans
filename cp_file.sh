0fc3a1bb13f2e5c7f1135161641d7a312aa7a96b
kidconan <hyskid801@126.com>
2020-05-18 09:56:52 +0800

    upload

---
+++ cp_file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp ./data_processing.py ../TransformerTTS/
+cp ./force_alignment.py ../TransformerTTS/
+cp ./prepare_data.py ../TransformerTTS/
+cp ./preprocess.py ../TransformerTTS/
+cp ./hyperparams.py ../TransformerTTS/
+
+cp ./hparams.py ../fast_speech_trans/
+cp ./dataset.py ../fast_speech_trans/
