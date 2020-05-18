8f269996a90b74d302380c4f143ecd265c18d213
kidconan <hyskid801@126.com>
2020-05-19 02:50:09 +0800

    upload

--- train_transformer.py
+++ train_transformer.py
@@ -39,7 +39,7 @@ def main():
     for epoch in range(hp.epochs):
 
         dataloader = DataLoader(dataset, batch_size=hp.batch_size, shuffle=True, collate_fn=collate_fn_transformer,
-                                drop_last=True, num_workers=4)
+                                drop_last=True, num_workers=1)
         pbar = tqdm(dataloader)
         for i, data in enumerate(pbar):
             pbar.set_description("Processing at epoch %d"%epoch)
