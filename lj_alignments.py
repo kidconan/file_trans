ffba9d709a4e502c331bc7fa2d4864b83629b594
kidconan <hyskid801@126.com>
2020-05-23 21:37:09 +0800

    upload

--- lj_alignments.py
+++ lj_alignments.py
@@ -1,10 +1,52 @@
 import os
-
-from preprocess import get_dataset, DataLoader, collate_fn_transformer
+from torch.utils.data import Dataset
+from preprocess import DataLoader, collate_fn_transformer
 from network import *
 from tensorboardX import SummaryWriter
 import torchvision.utils as vutils
 from tqdm import tqdm
+import librosa
+import pandas as pd
+from text import text_to_sequence
+
+
+class LJDatasets(Dataset):
+    """LJSpeech dataset."""
+
+    def __init__(self, csv_file, root_dir):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the wavs.
+
+        """
+        self.landmarks_frame = pd.read_csv(csv_file, sep='|', header=None)
+        self.root_dir = root_dir
+
+    def load_wav(self, filename):
+        return librosa.load(filename, sr=hp.sample_rate)
+
+    def __len__(self):
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, idx):
+        # wav_name = os.path.join(self.root_dir, self.landmarks_frame.ix[idx, 0][2:]) + '.wav'
+        wav_name = os.path.join(self.root_dir, self.landmarks_frame.ix[idx, 0]) + '.wav'
+        text = self.landmarks_frame.ix[idx, 1]
+
+        text = np.asarray(text_to_sequence(text, [hp.cleaners]), dtype=np.int32)
+        mel = np.load(wav_name[:-4] + '.pt.npy')
+        mel_input = np.concatenate([np.zeros([1,hp.num_mels], np.float32), mel[:-1,:]], axis=0)
+        text_length = len(text)
+        pos_text = np.arange(1, text_length + 1)
+        pos_mel = np.arange(1, mel.shape[0] + 1)
+
+        sample = {'text': text, 'mel': mel, 'text_length':text_length, 'mel_input':mel_input, 'pos_mel':pos_mel, 'pos_text':pos_text}
+
+        return sample
+
+def get_dataset():
+    return LJDatasets(os.path.join('./data/LJSpeech-1.1','metadata.csv'), os.path.join('./data/LJSpeech-1.1', 'wavs'))
 
 
 def get_D(alignment):
